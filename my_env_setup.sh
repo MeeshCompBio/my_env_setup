29b81450694ce7e27311e9ed2cc74d03b0ee6392
meeshcompbio <jmmichno2@gmail.com>
2020-03-30 13:40:46 -0500

    cleaning up

--- my_env_setup.sh
+++ my_env_setup.sh
@@ -4,53 +4,16 @@ WORKINGDIR=$PWD
 # install zsh
 cd
 echo 'Creating GitHub repo and cloning repos'
-mkdir -p GitHub
 mkdir -p Software
 mkdir -p $HOME/.local
 INSTALLATION_PATH="$HOME/.local"
-# Enter and clone necessary repos
-# cd $WORKINGDIR/GitHub
-# git clone https://github.com/tmux/tmux.git
-#git clone https://github.com/zsh-users/zsh.git
 
 cd $WORKINGDIR/Software
 wget https://hisham.hm/htop/releases/2.2.0/htop-2.2.0.tar.gz
 wget https://github.com/tmux/tmux/releases/download/2.4/tmux-2.4.tar.gz
 wget https://github.com/downloads/libevent/libevent/libevent-2.0.19-stable.tar.gz
-# wget ftp://ftp.gnu.org/gnu/ncurses/ncurses-5.9.tar.gz
 wget ftp://ftp.invisible-island.net/ncurses/ncurses.tar.gz
-# wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
-# wget http://ftp.gnu.org/gnu/automake/automake-1.11.6.tar.gz
-# wget http://mirror.jre655.com/GNU/libtool/libtool-2.4.6.tar.gz
-# wget https://github.com/downloads/libevent/libevent/libevent-2.0.19-stable.tar.gz
 wget -O zsh.tar.xz https://sourceforge.net/projects/zsh/files/latest/download
-# wget https://pkg-config.freedesktop.org/releases/pkg-config-0.29.2.tar.gz
-
-# # autoconf
-# tar xf autoconf*
-# cd autoconf-2.69
-# ./configure --prefix $INSTALLATION_PATH && make && make install
-# cd ..
-
-# #automake
-# tar xf automake*
-# cd automake-1.11.6
-# ./configure --prefix $INSTALLATION_PATH && make && make install
-# make install
-# cd ..
-
-# #libtool
-# tar xf libtool*
-# cd libtool-2.4.6
-# ./configure --prefix $INSTALLATION_PATH && make && make install
-# make install 
-# cd ..
-
-# #libevent
-# tar xvzf libevent-2.0.19-stable.tar.gz
-# cd libevent-2.0.19-stable
-# ./configure --prefix=$INSTALLATION_PATH && make && make install
-# cd ..
 
 # htop
 echo 'Installing htop'
@@ -59,11 +22,6 @@ rm htop-2.2.0.tar.gz
 cd htop-2.2.0
 ./configure --prefix=$PWD && make && make install
 cd ..
-
-# tar xvfz pkg-config-0.29.2.tar.gz
-# cd pkg-config-0.29.2
-# ./configure --prefix=$INSTALLATION_PATH --with-internal-glib && make && make install
-# cd ..
 
 ############
 # libevent #
@@ -74,17 +32,6 @@ cd libevent-2.0.19-stable
 make
 make install
 cd ..
-
-############
-# ncurses  #
-############
-# tar xvzf ncurses-5.9.tar.gz
-# cd ncurses-5.9
-# ./configure --prefix=$INSTALLATION_PATH
-# make
-# make install
-# cd ..
-
 
 # need ncurses
 echo "Installing ncurses"
@@ -100,20 +47,13 @@ export CFLAGS=-I$INSTALLATION_PATH/include
 export CPPFLAGS="-I$INSTALLATION_PATH/include" LDFLAGS="-L$INSTALLATION_PATH/lib"
 cd ..
 
-
+# Installing tmux
 tar xvzf tmux-2.4.tar.gz
 cd tmux-2.4
 ./configure CFLAGS="-I$INSTALLATION_PATH/include -I$INSTALLATION_PATH/include/ncurses" LDFLAGS="-L$INSTALLATION_PATH/lib -L$INSTALLATION_PATH/include/ncurses -L$INSTALLATION_PATH/include"
 CPPFLAGS="-I$INSTALLATION_PATH/include -I$INSTALLATION_PATH/include/ncurses" LDFLAGS="-static -L$INSTALLATION_PATH/include -L$INSTALLATION_PATH/include/ncurses -L$INSTALLATION_PATH/lib" make
 cp tmux $INSTALLATION_PATH/bin
 cd ..
-
-# # Go to github installs for tmux
-# cd $WORKINGDIR/GitHub/tmux
-# echo 'Installing tmux'
-# ./autogen.sh
-# ./configure --prefix=$INSTALLATION_PATH && make && make install
-# cd ..
 
 #############################
 # ZSH, OH MY ZSH AND PLUGINS
@@ -165,15 +105,3 @@ souce ~/.bashrc
 
 # adding a tmux conf file
 cat tmux_config_options.sh > ~/.tmux.conf
-
-# make local vimrc for my personal modifications
-# touch ~/.vimrc.local
-# touch ~/.vimrc.local.bundles
-# install all of the plugins
-# vim +PluginInstall +qall
-
-
-# # bashrc
-# echo 'modifying bashrc'
-# cd WORKINGDIR
-# cat bashrc_aliases >> .bashrc
